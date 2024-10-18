122100978fd467bf1620f540f3987390ad984c43
Imprez Ann L. Anda <andaimprezann@gmail.com>
2024-10-18 16:50:10 +0800

    Update and rename Student GradebookwithArrayProcessing to StudentGradebookwithArrayProcessing.java

--- StudentGradebookwithArrayProcessing.java
+++ StudentGradebookwithArrayProcessing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Student GradebookwithArrayProcessing {
+public class StudentGradebookwithArrayProcessing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
